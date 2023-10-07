c04a75eededd2224106e107c36f37bd72242d6a3
ArthurKaz <82533177+ArthurKaz@users.noreply.github.com>
2023-10-07 13:29:06 +0200

    Repository

---
+++ PDFBlazor/PDFOfferCreator/PDFOfferCreator.Api/Models/IOfferDataRepository.cs
@@ -0,0 +1,13 @@
+﻿using PDFOfferCreator.Model;
+
+namespace PDFOfferCreator.Api.Models
+{
+    public interface IOfferDataRepository
+    {
+        Task<IEnumerable<OfferData>> GetAllOfferDataAsync();
+        Task<OfferData> GetOfferDataByIdAsync(int id);
+        Task<OfferData> AddOfferDataAsync(OfferData offerData);
+        Task<OfferData> UpdateOfferDataAsync(OfferData offerData);
+        Task<OfferData> DeleteOfferDataAsync(int id);
+    }
+}
---
+++ PDFBlazor/PDFOfferCreator/PDFOfferCreator.Api/Models/OfferDataRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PDFOfferCreator.Model;
+
+namespace PDFOfferCreator.Api.Models
+{
+    public class OfferDataRepository : IOfferDataRepository
+    {
+        private readonly AppDbContext appDbContext;
+
+        public OfferDataRepository(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        public async Task<IEnumerable<OfferData>> GetAllOfferDataAsync()
+        {
+            return await appDbContext.OfferData.ToListAsync();
+        }
+
+        public async Task<OfferData> GetOfferDataByIdAsync(int id)
+        {
+            return await appDbContext.OfferData.FirstOrDefaultAsync(o => o.Id == id);
+        }
+
+        public async Task<OfferData> AddOfferDataAsync(OfferData offerData)
+        {
+            appDbContext.OfferData.Add(offerData);
+            await appDbContext.SaveChangesAsync();
+            return offerData;
+        }
+
+        public async Task<OfferData> UpdateOfferDataAsync(OfferData offerData)
+        {
+            var existingOfferData = await GetOfferDataByIdAsync(offerData.Id);
+            if (existingOfferData != null)
+            {
+                appDbContext.Entry(existingOfferData).CurrentValues.SetValues(offerData);
+                await appDbContext.SaveChangesAsync();
+            }
+            return existingOfferData;
+        }
+
+        public async Task<OfferData> DeleteOfferDataAsync(int id)
+        {
+            var offerData = await GetOfferDataByIdAsync(id);
+            if (offerData != null)
+            {
+                appDbContext.OfferData.Remove(offerData);
+                await appDbContext.SaveChangesAsync();
+            }
+            return offerData;
+        }
+    }
+}
